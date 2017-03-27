65445473ee06a823401a7e2f33f4e0dfc5a5dac8
Matthew Kirk Jensen <matthew.k.jensen@gmail.com>
2017-03-26 21:13:39 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+$('body').scrollspy({
+  target: '#myNavbar',
+  offset: 50
+})
+
+$("#myNavbar a").on('click', function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 500, function() {
+      window.location.hash = hash;
+    });
+  }
+
+});
