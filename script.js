8cfa66a6c73f20a093c9c8814368383d4565a732
Matthew <matthew.k.jensen@gmail.com>
2017-04-03 20:59:25 -0400

    Updated Project List

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $('body').scrollspy({
-  target: '#myNavbar',
-  offset: 50
+    target: '#myNavbar',
+    offset: 50
 })
 
 $("#myNavbar a").on('click', function(event) {
-  if (this.hash !== "") {
-    event.preventDefault();
-    var hash = this.hash;
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 500, function() {
-      window.location.hash = hash;
-    });
-  }
+    if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 500, function() {
+            window.location.hash = hash;
+        });
+    }
 
 });
